e6bcd5571a2324412ff350fae607be9829b9b869
Long Xu <xulongnr@gmail.com>
2015-08-26 15:38:45 +0800

    added myapp initial version

---
+++ myapp/www/js/directives.js
@@ -0,0 +1,46 @@
+angular.module('starter.directives', [])
+
+.directive('map', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      onCreate: '&'
+    },
+    link: function ($scope, $element, $attr) {
+      function initialize() {
+        var map = new BMap.Map($element[0]);
+
+        var geolocationControl = new BMap.GeolocationControl();
+        geolocationControl.addEventListener("locationSuccess", function(e) {
+
+        });
+      
+        geolocationControl.addEventListener("locationError", function(e) {
+          alert(e.message);
+        });
+        map.addControl(geolocationControl);
+
+        var geolocation = new BMap.Geolocation();
+        geolocation.getCurrentPosition(function(r) {
+            if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+              var marker = new BMap.Marker(r.point);
+              map.addOverlay(marker);
+              map.centerAndZoom(r.point, 16);
+
+            } else {
+              alert('failed' + this.getStatus());
+            }
+        },{enableHighAccuracy: false});
+    	 
+
+//      var marker = new BMap.Marker(point);
+//      map.addOverlay(marker);
+//      $scope.onCreate({map: map});
+//      $scope.map = map;
+      }
+
+      window.onload = initialize;
+      window.onreload = initialize;
+    }
+  }
+});
