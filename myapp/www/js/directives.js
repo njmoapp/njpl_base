52ebc0cbe93e08e357b07d472f5dfd67b5ed12e7
XuLong <xulongnr@gmail.com>
2015-09-02 16:59:12 +0800

    Fixed map refresh issue

--- myapp/www/js/directives.js
+++ myapp/www/js/directives.js
@@ -1,43 +1,81 @@
 angular.module('starter.directives', [])
 
-.directive('map', function() {
+.directive('appMap', function() {
   return {
     restrict: 'E',
+    replace: true,
+    template: "<div id='allMap'></div>",
     scope: {
-      onCreate: '&'
+      onCreate: '&',
+      center: "=",
+      markers: "=",
+      width: "@",
+      height: "@",
+      scaleControl: "@",
+      address: "@"
     },
+
     link: function ($scope, $element, $attr) {
-      function initialize() {
-        var map = new BMap.Map($element[0]);
+      var map = new BMap.Map("allMap");
+      var myGeo = new BMap.Geocoder();
+      var geolocationControl = new BMap.GeolocationControl();
+      geolocationControl.addEventListener("locationSuccess", function(e) {
+      });
+      geolocationControl.addEventListener("locationError", function(e) {
+        alert(e.message);
+      });
+      map.addControl(geolocationControl);
 
-        var geolocationControl = new BMap.GeolocationControl();
-        geolocationControl.addEventListener("locationSuccess", function(e) {
+      myGeo.getPoint($scope.address, function(point) {
+        if (point) {
+          map.centerAndZoom(point, 16);
+          map.addOverlay(new BMap.Marker(point));
+        }
+      }, "");
 
-        });
-      
-        geolocationControl.addEventListener("locationError", function(e) {
-          alert(e.message);
-        });
-        map.addControl(geolocationControl);
-
-        var geolocation = new BMap.Geolocation();
-        geolocation.getCurrentPosition(function(r) {
-            if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+      var geolocation = new BMap.Geolocation();
+      geolocation.getCurrentPosition(function(r) {
+         if (this.getStatus() == BMAP_STATUS_SUCCESS) {
               var marker = new BMap.Marker(r.point);
               map.addOverlay(marker);
               map.centerAndZoom(r.point, 16);
 
-            } else {
+         } else {
               alert('failed' + this.getStatus());
-            }
-        },{enableHighAccuracy: false});
+         }
+      }, {enableHighAccuracy: true});
 
-        $scope.onCreate({map: map});
-//      $scope.map = map;
-      }
+      $scope.onCreate({map: map});
 
-      window.onload = initialize;
-//    window.onreload = initialize;
+      // function initialize() {
+      //   var map = new BMap.Map($element[0]);
+
+      //   var geolocationControl = new BMap.GeolocationControl();
+      //   geolocationControl.addEventListener("locationSuccess", function(e) {
+
+      //   });
+      
+      //   geolocationControl.addEventListener("locationError", function(e) {
+      //     alert(e.message);
+      //   });
+      //   map.addControl(geolocationControl);
+
+      //   var geolocation = new BMap.Geolocation();
+      //   geolocation.getCurrentPosition(function(r) {
+      //       if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+      //         var marker = new BMap.Marker(r.point);
+      //         map.addOverlay(marker);
+      //         map.centerAndZoom(r.point, 16);
+
+      //       } else {
+      //         alert('failed' + this.getStatus());
+      //       }
+      //   },{enableHighAccuracy: false});
+
+      //   $scope.onCreate({map: map});
+      // }
+
+      // window.onload = initialize;
     }
   }
 });
