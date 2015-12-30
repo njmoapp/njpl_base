636e97103dc283d847c06f194cd7571c27a2e322
Xu Long <xulongnr@gmail.com>
2015-12-30 14:32:04 +0800

    Init version of myappui

---
+++ myappui/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('mapCtrl', function($scope) {
+
+})
+   
+.controller('orderHistoryCtrl', function($scope) {
+
+})
+ 
---
+++ myappui/www/js/routes.js
@@ -0,0 +1,75 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+    .state('parking', {
+      url: '/side-menu21',
+      abstract:true,
+      templateUrl: 'templates/parking.html'
+    })
+      
+    
+      
+        
+    .state('parking.login', {
+      url: '/page1',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/login.html',
+          controller: 'loginCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('signup', {
+      url: '/page2',
+      templateUrl: 'templates/signup.html',
+      controller: 'signupCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('parking.map', {
+      url: '/page3',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/map.html',
+          controller: 'mapCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('parking.orderHistory', {
+      url: '/page4',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/orderHistory.html',
+          controller: 'orderHistoryCtrl'
+        }
+      }
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/side-menu21/page3');
+
+});
